b6b7398bdd915970c64569656fb68ab86bd34d7e
MonsterDruide1 <5958456@gmail.com>
2025-09-18 02:35:30 +0200

    more parsing

--- src/nso.rs
+++ src/nso.rs
@@ -1,7 +1,7 @@
 use std::{fs::File, io::{Read, Seek, Cursor}};
 
 use anyhow::ensure;
-use binrw::{binread, BinRead};
+use binrw::{binread, BinRead, BinReaderExt, NullString};
 use num_enum::TryFromPrimitive;
 use sha2::{Digest, Sha256};
 
@@ -14,6 +14,11 @@ pub struct NSO {
     pub data_segment: Vec<u8>,
     pub build_str: BuildStr,
     pub symbol_table: Vec<DynamicSymbol>,
+    pub module: Module,
+    pub reloc_dyn_table: Vec<Relocation>,
+    pub reloc_plt_table: Vec<Relocation>,
+    pub dynstr_table: Vec<String>,
+    pub global_plt: Vec<u64>,
 }
 
 impl NSO {
@@ -26,6 +31,24 @@ impl NSO {
 
         let build_str = BuildStr::new(&rodata_segment)?;
         let symbol_table = Self::parse_dynamic_symbols(&rodata_segment, header.dynsym_offset, header.dynsym_size)?;
+        let module = Module::read_le(&mut Cursor::new(&text_segment))?;
+        let dynamic_segment = Self::parse_dynamic_section(
+            &data_segment[ (module.header_offset + module.dyn_offset - header.get_segment_mem_offset(&NsoSegment::Data)) as usize ..]
+        )?;
+        // skip .hash and .gnu_hash for now
+        let reloc_dyn_table = Self::parse_reloc_table(
+            &rodata_segment, &dynamic_segment, DynamicTagType::DT_RELA,
+            DynamicTagType::DT_RELASZ, &header
+        )?;
+        let reloc_plt_table = Self::parse_reloc_table(
+            &rodata_segment, &dynamic_segment, DynamicTagType::DT_JMPREL,
+            DynamicTagType::DT_PLTRELSZ, &header
+        )?;
+        let dynstr_table = Self::parse_dynamic_string_table(&rodata_segment, header.dynstr_offset, header.dynstr_size)?;
+        let global_plt = Self::parse_global_plt(
+            &rodata_segment[dynamic_segment.len()*16 .. ],
+            reloc_plt_table.iter().filter(|r| r.reloc_type == RelocationType::R_AARCH64_JUMP_SLOT).count()
+        )?;
 
         Ok(NSO {
             header,
@@ -34,6 +57,11 @@ impl NSO {
             data_segment,
             build_str,
             symbol_table,
+            module,
+            reloc_dyn_table,
+            reloc_plt_table,
+            dynstr_table,
+            global_plt,
         })
     }
 
@@ -66,6 +94,74 @@ impl NSO {
         }
         Ok(symbols)
     }
+
+    fn parse_dynamic_section(data: &[u8]) -> anyhow::Result<Vec<(DynamicTagType, u64)>> {
+        let mut tags = Vec::new();
+        let mut cursor = Cursor::new(data);
+        loop {
+            let tag = u64::read_le(&mut cursor)?;
+            let val = u64::read_le(&mut cursor)?;
+            if tag == DynamicTagType::DT_NULL as u64 {
+                break;
+            }
+            tags.push((DynamicTagType::try_from(tag)?, val));
+        }
+        Ok(tags)
+    }
+
+    fn get_dynamic_tag_value(dynamic_segment: &[(DynamicTagType, u64)], tag_type: DynamicTagType) -> anyhow::Result<u64> {
+        // error if none or multiple found
+        let mut values = dynamic_segment.iter().filter(|(t, _)| *t == tag_type).map(|(_, v)| *v);
+        let first = values.next().ok_or_else(|| anyhow::anyhow!("Dynamic tag {:?} not found", tag_type))?;
+        ensure!(values.next().is_none(), "Multiple dynamic tags {:?} found", tag_type);
+        Ok(first)
+    }
+
+    fn parse_reloc_table(
+        rodata_segment: &[u8], dynamic_segment: &[(DynamicTagType, u64)],
+        off_tag: DynamicTagType, size_tag: DynamicTagType, header: &NsoHeader
+    ) -> anyhow::Result<Vec<Relocation>> {
+        let rela_offset = (Self::get_dynamic_tag_value(dynamic_segment, off_tag)?
+            - header.get_segment_mem_offset(&NsoSegment::Rodata) as u64) as usize;
+        let rela_size = Self::get_dynamic_tag_value(dynamic_segment, size_tag)? as usize;
+        let rela_ent = Self::get_dynamic_tag_value(dynamic_segment, DynamicTagType::DT_RELAENT)? as usize;
+        ensure!(rela_ent == std::mem::size_of::<Relocation>(), "Unexpected DT_RELAENT size");
+
+        let num_relocs = rela_size / rela_ent;
+        let mut relocations = Vec::with_capacity(num_relocs);
+        for i in 0..num_relocs {
+            let offset = rela_offset + i * rela_ent;
+            let data = &rodata_segment[offset..offset + rela_ent];
+            let mut cursor = Cursor::new(data);
+            let reloc = Relocation::read_le(&mut cursor)?;
+            relocations.push(reloc);
+        }
+        Ok(relocations)
+    }
+
+    fn parse_dynamic_string_table(rodata_segment: &[u8], dynstr_offset: u32, dynstr_size: u32) -> anyhow::Result<Vec<String>> {
+        let str_data = &rodata_segment[dynstr_offset as usize .. (dynstr_offset + dynstr_size) as usize];
+        let mut cursor = Cursor::new(str_data);
+        let mut strings = Vec::new();
+        loop {
+            if cursor.position() as usize >= str_data.len() {
+                break;
+            }
+            strings.push(cursor.read_le::<NullString>()?.to_string());
+        }
+        Ok(strings)
+    }
+
+    fn parse_global_plt(data: &[u8], num_entries: usize) -> anyhow::Result<Vec<u64>> {
+        let data_without_header = &data[0x18..];
+        let mut plt_entries = Vec::with_capacity(num_entries);
+        let mut cursor = Cursor::new(data_without_header);
+        for _ in 0..num_entries {
+            let entry = u64::read_le(&mut cursor)?;
+            plt_entries.push(entry);
+        }
+        Ok(plt_entries)
+    }
 }
 
 #[derive(Debug)]
@@ -92,29 +188,32 @@ pub struct DynamicSymbol {
 }
 #[repr(u8)]
 #[derive(Debug, TryFromPrimitive)]
+#[allow(non_camel_case_types)]
 pub enum DynamicSymbolBind {
-    Local = 0,
-    Global = 1,
-    Weak = 2,
+    STB_LOCAL = 0,
+    STB_GLOBAL = 1,
+    STB_WEAK = 2
 }
 #[repr(u8)]
 #[derive(Debug, TryFromPrimitive)]
+#[allow(non_camel_case_types)]
 pub enum DynamicSymbolType {
-    NoType = 0,
-    Object = 1,
-    Func = 2,
-    Section = 3,
-    File = 4,
-    Common = 5,
-    TLS = 6,
+    STT_NOTYPE = 0,
+    STT_OBJECT = 1,
+    STT_FUNC = 2,
+    STT_SECTION = 3,
+    STT_FILE = 4,
+    STT_COMMON = 5,
+    STT_TLS = 6
 }
 #[repr(u8)]
 #[derive(Debug, TryFromPrimitive)]
+#[allow(non_camel_case_types)]
 pub enum DynamicSymbolVisibility {
-    Default = 0,
-    Internal = 1,
-    Hidden = 2,
-    Protected = 3,
+    STV_DEFAULT = 0,
+    STV_INTERNAL = 1,
+    STV_HIDDEN = 2,
+    STV_PROTECTED = 3
 }
 impl DynamicSymbol {
     pub fn get_bind(&self) -> anyhow::Result<DynamicSymbolBind> {
@@ -126,4 +225,103 @@ impl DynamicSymbol {
     pub fn get_visibility(&self) -> anyhow::Result<DynamicSymbolVisibility> {
         Ok(DynamicSymbolVisibility::try_from(self.other & 0x03)?)
     }
+}
+
+#[binread]
+#[derive(Debug)]
+pub struct Module {
+    #[br(temp)]
+    _0: u32,  // might be version
+    pub header_offset: u32,
+    #[br(magic = b"MOD0")]
+    // all of these are relative to beginning of module => add header_offset
+    pub dyn_offset: u32,
+    pub bss_start: u32,
+    pub bss_end: u32,
+    pub ex_info_start_offset: u32,
+    pub ex_info_end_offset: u32,
+    #[br(temp, assert(module_offset == bss_start))]
+    module_offset: u32,
+}
+
+
+#[repr(u64)]
+#[derive(Debug, TryFromPrimitive, PartialEq)]
+#[allow(non_camel_case_types)]
+pub enum DynamicTagType {
+    DT_NULL = 0,
+    DT_NEEDED = 1,
+    DT_PLTRELSZ = 2,
+    DT_PLTGOT = 3,
+    DT_HASH = 4,
+    DT_STRTAB = 5,
+    DT_SYMTAB = 6,
+    DT_RELA = 7,
+    DT_RELASZ = 8,
+    DT_RELAENT = 9,
+    DT_STRSZ = 10,
+    DT_SYMENT = 11,
+    DT_INIT = 12,
+    DT_FINI = 13,
+    DT_SONAME = 14,
+    DT_RPATH = 15,
+    DT_SYMBOLIC = 16,
+    DT_REL = 17,
+    DT_RELSZ = 18,
+    DT_RELENT = 19,
+    DT_PLTREL = 20,
+    DT_DEBUG = 21,
+    DT_TEXTREL = 22,
+    DT_JMPREL = 23,
+    DT_BIND_NOW = 24,
+    DT_INIT_ARRAY = 25,
+    DT_FINI_ARRAY = 26,
+    DT_INIT_ARRAYSZ = 27,
+    DT_FINI_ARRAYSZ = 28,
+    DT_RUNPATH = 29,
+    DT_FLAGS = 30,
+    DT_PREINIT_ARRAY = 32,
+    DT_PREINIT_ARRAYSZ = 33,
+    DT_NUM = 34,
+    DT_LOOS = 0x6000000d,
+    DT_HIOS = 0x6ffff000,
+    DT_LOPROC = 0x70000000,
+    DT_HIPROC = 0x7fffffff,
+    DT_ADDRRNGLO = 0x6ffffe00,
+    DT_GNU_HASH = 0x6ffffef5,
+    DT_TLSDESC_PLT = 0x6ffffef6,
+    DT_TLSDESC_GOT = 0x6ffffef7,
+    DT_GNU_CONFLICT = 0x6ffffef8,
+    DT_GNU_LIBLIST = 0x6ffffef9,
+    DT_CONFIG = 0x6ffffefa,
+    DT_DEPAUDIT = 0x6ffffefb,
+    DT_AUDIT = 0x6ffffefc,
+    DT_PLTPAD = 0x6ffffefd,
+    DT_MOVETAB = 0x6ffffefe,
+    DT_SYMINFO = 0x6ffffeff,
+    DT_RELACOUNT = 0x6ffffff9,
+    DT_RELCOUNT = 0x6ffffffa
+}
+
+#[binread]
+#[derive(Debug)]
+pub struct Relocation {
+    pub offset: u64,
+    pub reloc_type: RelocationType,
+    pub sym_idx: u32,  // maybe these two are in the wrong order
+    pub addend: i64,
+}
+
+#[derive(Debug, BinRead, PartialEq)]
+#[br(repr = u32)]
+#[allow(non_camel_case_types)]
+pub enum RelocationType {
+    R_AARCH64_COPY = 1024,
+    R_AARCH64_GLOB_DAT = 1025,
+    R_AARCH64_JUMP_SLOT = 1026,
+    R_AARCH64_RELATIVE = 1027,
+    R_AARCH64_TLS_TPREL64 = 1030,
+    R_AARCH64_TLS_DTPREL32 = 1031,
+    R_AARCH64_IRELATIVE = 1032,
+    R_AARCH64_ABS64 = 257
 }
