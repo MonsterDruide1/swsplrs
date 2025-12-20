fcc304e5fd45d262f0b2c61566b6cd50581bb51f
MonsterDruide1 <5958456@gmail.com>
2025-12-20 18:10:03 +0100

    add more stubs

---
+++ assets/glslcStubs.c
@@ -0,0 +1,9 @@
+
+void* glslcCompilePreSpecialized;
+void* glslcCompileSpecialized;
+void* glslcInitialize;
+void* glslcFinalize;
+void* glslcCompile;
+void* glslcGetVersion;
+void* glslcSetAllocator;
+void* glslcGetDefaultOptions;
