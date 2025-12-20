f7679e9f9a32dfdbd0eefa4de9c4c46747db9106
MonsterDruide1 <5958456@gmail.com>
2025-12-20 18:06:43 +0100

    add more stubs

---
+++ assets/cxxstubs.c
@@ -0,0 +1,2 @@
+int __dso_handle;
+int __cxa_atexit(void (*f)(void *), void *p, void *d) { return 0; }
---
+++ assets/missingfunctions.cpp
@@ -0,0 +1,9 @@
+namespace sead {
+template <typename T>
+class BufferedSafeStringBase;
+template <typename T>
+class SafeStringBase;
+}
+
+void FUN_710086f65c(sead::BufferedSafeStringBase<char>* out,
+                    const sead::SafeStringBase<char>& envStr) {}
