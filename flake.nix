d0b00b1f7aa91b0fde8e21da9e0b3049a97209a6
MonsterDruide1 <5958456@gmail.com>
2026-01-02 13:42:03 +0100

    update nix flake

--- flake.nix
+++ flake.nix
@@ -24,6 +24,8 @@
           pkgs.pkgsCross.aarch64-multiplatform.buildPackages.binutils
           (pkgs.writeShellScriptBin "aarch64-linux-gnu-as" "aarch64-unknown-linux-gnu-as")
           (pkgs.writeShellScriptBin "aarch64-linux-gnu-ld" "aarch64-unknown-linux-gnu-ld")
+          pkgs.ncurses5
+          pkgs.ncurses6
         ];
         LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath buildInputs}";
       };
