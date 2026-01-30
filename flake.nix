2a959d105e11f28d28ea0e8ad2128ad688ba1e8e
MonsterDruide1 <5958456@gmail.com>
2026-01-30 13:26:16 +0100

    nix: add stripping to flake

--- flake.nix
+++ flake.nix
@@ -24,6 +24,7 @@
           pkgs.pkgsCross.aarch64-multiplatform.buildPackages.binutils
           (pkgs.writeShellScriptBin "aarch64-linux-gnu-as" "aarch64-unknown-linux-gnu-as")
           (pkgs.writeShellScriptBin "aarch64-linux-gnu-ld" "aarch64-unknown-linux-gnu-ld")
+          (pkgs.writeShellScriptBin "aarch64-linux-gnu-strip" "aarch64-unknown-linux-gnu-strip")
           pkgs.ncurses5
           pkgs.ncurses6
         ];
