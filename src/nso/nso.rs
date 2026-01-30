b873ee461b0a7cdcd14f3690d1a4558a0a959e00
MonsterDruide1 <5958456@gmail.com>
2026-01-30 14:21:28 +0100

    objdiff: sort `.[ro]data` by target, not source

--- src/nso/nso.rs
+++ src/nso/nso.rs
@@ -1403,7 +1403,7 @@ impl NSO {
             Embed,
         }
 
-        refs.sort_by_cached_key(|(s, _)| *s);
+        refs.sort_by_cached_key(|(_, t)| *t);
 
         let mut chunks = HashMap::new();
         let rodata_subsections = hacks.get_rodata_subsections();
